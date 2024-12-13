e5f15aefb1cbb1392756454285b3d874ed2cccc2
AMVMesias <mesiasv2@gmail.com>
2024-12-13 10:56:20 -0500

    2P JS

---
+++ 2P/Introduccion Javascript/SRC/firs.js
@@ -0,0 +1,11 @@
+var nombre;
+var edad;
+var altura;
+
+nombre = "Juan";
+edad = 25;
+altura = 1.75;
+
+console.log("Nombre: " + nombre);
+console.log("Edad: " + edad);
+console.log("Altura: " + altura);
